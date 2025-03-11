e7596283f6dbb9ea82b30d9ee4d501b975d1ba47
Kieran Hanrahan <kieran@knhn.me>
2025-03-10 23:50:58 -0400

    Bump version

--- set_in_out_marks.py
+++ set_in_out_marks.py
@@ -2,7 +2,7 @@
 Script Name: Set In & Out Marks
 Written by: Kieran Hanrahan
 
-Script Version: 2.0.0
+Script Version: 3.0.0
 Flame Version: 2025
 
 URL: http://www.github.com/khanrahan/set-in-out-marks
@@ -36,7 +36,7 @@ import flame
 from PySide6 import QtCore, QtGui, QtWidgets
 
 TITLE = 'Set In and Out Marks'
-VERSION_INFO = (2, 0, 0)
+VERSION_INFO = (3, 0, 0)
 VERSION = '.'.join([str(num) for num in VERSION_INFO])
 TITLE_VERSION = f'{TITLE} v{VERSION}'
 MESSAGE_PREFIX = '[PYTHON]'
